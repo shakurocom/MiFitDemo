8f8728aa0318df12baf4f6168f531baf56cc73f8
EugeneKlyuenkov <dawd>
2022-06-22 10:45:59 +0300

    Initial Commit

---
+++ MiFit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MiFit
+//
+//  Created by Eugene Klyuenkov on 22.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
